3ff7295aeb69952be0b1d55ce08c63d100a32f85
psy <psy@psys-MacBook-Air.local>
2026-02-14 12:51:21 -0500

    transformer friendly

--- Transformer/test_dataset.py
+++ Transformer/test_dataset.py
@@ -14,11 +14,13 @@ class MockTokenizer:
 
 
 def test_text_dataset_basic():
-    dataset = TextDataset("hello world test", MockTokenizer())
+    dataset = TextDataset("hello world test", MockTokenizer(), seq_len=2)
 
     assert dataset.tokens == [1, 2, 3]
-    assert dataset.inputs == [1, 2]
-    assert dataset.targets == [2, 3]
-    assert len(dataset) == 2
-    assert dataset[0] == (1, 2)
-    assert dataset[1] == (2, 3)
+
+    assert dataset.inputs == [[1, 2]]
+    assert dataset.targets == [[2, 3]]
+
+    assert len(dataset) == 1
+
+    assert dataset[0] == ([1, 2], [2, 3])
